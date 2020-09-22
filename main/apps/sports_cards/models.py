8bebae67001b6175a5d32618ac1c3c714f8c1647
kyle-deleongit config --global user.email kydeleon10@gmail.comgit config --global --listgit config --global user.name kyle-deleon <kydeleon10@gmail.com>
2020-09-22 10:48:12 -0700

    created classes

--- main/apps/sports_cards/models.py
+++ main/apps/sports_cards/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.Charfield(max_length=255)
+    last_name = models.Charfield(max_length=255)
+    email = models.Charfield(max_length=255)
+    password = models.Charfield(max_length=255)
+    user_name = models.Charfield(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Cards(models.Model):
+    year = models.DecimalField() 
+    make = models.Charfield(max_length=255)
+    card_number = models.DecimalField()
+    name = models.Charfield(max_length=255)
+    special = models.Charfield(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    uploaded_by = models.ForeignKey(User, related_name="has_cards", on_delete=models.CASCADE)
+    liked_by = models.ManyToManyField(User, related_name="has_likes")
+
+class Review(models.Model):
+    content = models.TextField()
+    rating = models.IntergerField(min=1,max=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    reviewed_by = models.ForeignKey(User, related_name="has_reviews", on_delete=models.CASCADE)
+    cards = models.ForeignKey(Cards, related_name="has_reviews", on_delete=models.CASCADE)
+
+
+
